3ac7d192a939d1500dee10e82ffb734fb168ea9b
DESKTOP-JJSLHJQ\angry <shalevamit9@gmail.com>
2021-04-15 03:09:54 +0300

    initial commit

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import styles from './Burger.module.css';
+
+/**
+ * @typedef {Object} BurgerProps
+ */
+
+/**
+ * @param {BurgerProps} props 
+ */
+const Burger = props => {
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default Burger;
